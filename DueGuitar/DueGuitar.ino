6b734707cce8ddf4a45dc162fb313c8898e1a104
WaveShapePlay <WaveShapePlay@gmail.com>
2017-08-08 05:57:58 -0400

    Create DueGuitar.ino

---
+++ DueGuitar/DueGuitar.ino
@@ -0,0 +1,29 @@
+
+int in_ADC0, in_ADC1;  
+
+
+
+void setup()
+{
+  //ADC Configuration
+  ADC->ADC_MR |= 0x80;   // ADC in free running mode.
+  ADC->ADC_CR=2;         // Starts ADC conversion.
+  ADC->ADC_CHER=0x80;  // Enable ADC channel 0
+
+  //DAC Configuration
+  analogWrite(DAC0,0);  // Enables DAC0
+
+}
+
+void loop()
+{
+  //Read the ADCs
+  while((ADC->ADC_ISR & 0x80)==0);
+  in_ADC0=ADC->ADC_CDR[7];               // read data from ADC0
+
+  
+  //Write the DACs
+  dacc_set_channel_selection(DACC_INTERFACE, 0);       //select DAC channel 0
+  dacc_write_conversion_data(DACC_INTERFACE, in_ADC0);//write on DAC
+
+}
