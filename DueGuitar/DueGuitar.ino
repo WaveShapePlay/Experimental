4cffeb05f7b31d21d0ef7a3a5706334391edafc8
WaveShapePlay <WaveShapePlay@gmail.com>
2017-08-10 06:28:13 -0400

    Update DueGuitar.ino

--- DueGuitar/DueGuitar.ino
+++ DueGuitar/DueGuitar.ino
@@ -4,23 +4,23 @@ int in_ADC0;
 
 void setup()
 {
-  //ADC Configuration
+  
   ADC->ADC_MR |= 0x80;        // Mode Register - Select free running mode for ADC.
   ADC->ADC_CR = 0x02;         // Control Register - Begin conversion
-  ADC->ADC_CHER = 0x80;       // Enable ADC channel 0
-
-  //DAC Configuration
-  analogWrite(DAC0,0);  // Enables DAC0
+  ADC->ADC_CHER = 0x80;       // Channel Enable Register - Selects the correstponding ADC channels 
+                              // A0 = CH7, A1 = CH6, A2 = CH5, A3 = CH4
+                              // For example A0 on the Arduino Due board uses the internal CH7 of ADC
+  
+  analogWrite(DAC0,0);        // Enables Digital to Analog Converter 
+  
 }
 
 void loop()
 {
-  //Read the ADCs
-  while((ADC->ADC_ISR & 0x80) != 0x80);   // Single statment - wait for ADC conversion of ADC0
-  in_ADC0=ADC->ADC_CDR[7];               // read data from ADC0
+                                          
+  while((ADC->ADC_ISR & 0x80) != 0x80);   // Single statment - Wait for ADC CH7 End of Conversion
+  in_ADC0=ADC->ADC_CDR[7];                // Read CH7 ADC and store in in_ADC0
 
-  
-  //Write the DACs
-  dacc_set_channel_selection(DACC_INTERFACE, 0);       //select DAC channel 0
-  dacc_write_conversion_data(DACC_INTERFACE, in_ADC0);//write on DAC
+  dacc_set_channel_selection(DACC_INTERFACE, 0);        // Select channel D0 for DAC
+  dacc_write_conversion_data(DACC_INTERFACE, in_ADC0);  // Write in_ADC0 to DAC
 }
