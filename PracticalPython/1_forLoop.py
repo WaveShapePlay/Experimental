a9ade972770d1ead20930e957a51baf58ce6ddb5
WaveShapePlay <WaveShapePlay@gmail.com>
2017-09-29 06:32:10 -0400

    Create 1_forLoop.py

---
+++ PracticalPython/1_forLoop.py
@@ -0,0 +1,20 @@
+
+print('Example 1:')
+
+for x in range(0,10):
+    print(x)
+    
+print('\n')
+print('Example 2:')
+
+for u in range(0,5):
+    print(2**u)
+
+print('\n')
+print('Example 3:')
+
+sal = 50000
+# Printing out 5% rasis over 4 years
+for y in range(0,4):
+    sal = sal + sal*0.05
+    print(sal)
