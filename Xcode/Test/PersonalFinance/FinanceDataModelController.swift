558ac29dad0b27af11d89dde2750c747a95de713
WaveShapePlay <WaveShapePlay@gmail.com>
2020-07-10 18:11:14 -0400

    Add files via upload

---
+++ Xcode/Test/PersonalFinance/FinanceDataModelController.swift
@@ -0,0 +1,13 @@
+//
+//  FinanceDataModelController.swift
+//  PersonalFinance
+//
+//  Created by Robert John on 7/10/20.
+//  Copyright © 2020 Robert John. All rights reserved.
+//
+
+import Foundation
+
+class FinanceModelController{
+    var Fina = financeData(salary: 80000,principal: 100000, intrest: 0.07)
+}
