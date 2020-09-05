b60414668fe8c70dc0743b507712270fe32d6f5e
WaveShapePlay <WaveShapePlay@gmail.com>
2020-09-05 09:17:51 -0400

    Add files via upload

---
+++ BlinkGUI_V1.1/Arduino_BlinkGUI_1/Arduino_BlinkGUI_1.ino
@@ -0,0 +1,71 @@
+
+int analogPin = 3;     
+int data = 0;           
+char userInput;
+
+void setup(){
+
+  Serial.begin(9600);                        //  setup serial
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+// for getDelayTime - PySerial will nee to send both 'b' followed by the time.
+int getDelayTime(){
+  Serial.print("In the delay time fuction");
+  String blinkTimeString;
+  int blinkTimeInt = 0;
+  
+  delay(2000); // Need to wait for pyserial send time 
+  blinkTimeString = Serial.readString();
+  blinkTimeInt = blinkTimeString.toInt();
+  return blinkTimeInt;
+
+} // Get Delay Time Loop function
+
+int getBlinkLoopNum(){
+  Serial.print("In the blink loop fuction");
+  String numLoopString;
+  int numLoopInt = 0;
+  
+  delay(2000); // Need to wait for pyserial send time 
+  numLoopString = Serial.readString();
+  numLoopInt = numLoopString.toInt();
+  return numLoopInt;
+}// Get blink index function
+
+
+void loop(){
+  
+if(Serial.available()> 0){ 
+    
+    userInput = Serial.read();               // read user input
+      
+      if(userInput == 'o'){                
+        digitalWrite(LED_BUILTIN, HIGH); 
+      }
+      if(userInput == 'x'){
+       digitalWrite(LED_BUILTIN, LOW);         
+      }
+      if(userInput == 'b'){
+        
+        int delayTime = getDelayTime();
+        String delayTimePrint = String(delayTime);
+        Serial.println("The time recived was: ");
+        Serial.println(delayTimePrint);
+
+        // Use this function to get uset input on number off loops to run
+        //int numLoops = getBlinkLoopNum()
+        
+        for(int i =0;i<10;i++){
+          digitalWrite(LED_BUILTIN,LOW);
+          delay(delayTime);
+          digitalWrite(LED_BUILTIN,HIGH);
+          delay(delayTime);
+          
+        } // User delay blink for loop
+        digitalWrite(LED_BUILTIN,LOW);
+      } // if userinput == 'b'
+  } // Serial.available
+} // Void Loop
+
+  
